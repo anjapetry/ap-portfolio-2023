b3d8860fc99ee5b70c0eccfe77b423d93529fd8d
AnjaP <treyjapan@pm.me>
2023-09-08 12:37:35 +0200

    feat:add header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import  Header from "./components/header";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,11 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-100 text-slate-950`}>
+      <body className={`${inter.className} bg-slate-100 text-slate-950 relative h-[5000px]`}>
         <div className='bg-[#efb3b5] absolute top-[-6rem] right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className="bg-[#aba5de] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <Header />
         {children}
         </body>
     </html>
-  )
+  );
 }
