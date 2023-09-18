a850de2538d1d6e838c90be12db4f9d58ed4c13a
AnjaP <treyjapan@pm.me>
2023-09-18 19:13:48 +0200

    feat:implement looping section divider

---
+++ src/app/components/section-divider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function SectionDivider() {
+  return (
+    <motion.div
+    className='w-full flex justify-center items-center'
+    initial={{ opacity: 0, y: 100 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ delay: 0.125 }}
+    >
+
+  <div className='my-24 w-[33px] h-[64px] rounded-3xl border-2 border-fuchsia-700 sm:block p-2'>
+    <motion.div
+      animate={{
+        y: [0, 24, 0],
+      }}
+      transition={{
+        duration: 1.5,
+        repeat: Infinity,
+        repeatType: "loop",
+      }}
+      className='w-3 h-3 rounded-full bg-teal-400 mb-1'
+    >
+    </motion.div>
+    </div>
+    </motion.div>
+);
+}
+
